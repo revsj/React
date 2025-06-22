c68f65807fada4fdc37d3d4e58296730bb77ea62
revsj <rexjalandoni08@gmail.com>
2025-06-22 18:26:37 +0800

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import RecipeTitle from './RecipeTitle'
+import './index.css';   
+
+function App() {
+    return (
+        <article>
+            <h1>Hello React! </h1>\
+            <h2>revs</h2>
+            <h3>Welcome to the world of React</h3>
+            <RecipeTitle  />
+        </article>
+       
+    )
+}
+
+export default App;
